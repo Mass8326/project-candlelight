696c76ff4d067e45411ee6cfae3f678d077ab8d1
Mass8326 <reyf.mag@gmail.com>
2018-03-01 19:53:36 -0500

    Add data import and export

--- src/app/sheet/sheet.component.ts
+++ src/app/sheet/sheet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { Store } from "../store";
 import { Sheet } from "./sheet";
 import { StoreService } from '../store.service';
 
@@ -17,5 +18,53 @@ export class SheetComponent implements OnInit {
   // Initialization
   public ngOnInit () : void {
     this.sheet = this.storeService.getSheet();
+  }
+  // Event handlers
+  public triggerExport () : void {
+    // Parse store into embedded json
+    let fileName:string = this.sheet.title
+      .replace(/[\W_]/g, '')                        // remove non alphanumerics
+      .toLowerCase() + '.'                          // lowercase and separator
+      + Math.round((new Date()).getTime() / 1000)   // unix time
+      + '.candle.json';                             // extension
+    let dataRaw:Store = this.storeService.getData();
+    let dataStr:string = JSON.stringify(dataRaw);
+    let dataUri:string = 'data:application/json;charset=utf-8,'
+      + encodeURIComponent(dataStr);
+    // Create, click, and remove temporary anchor element
+    let elem:HTMLAnchorElement = document.createElement('a');
+    elem.setAttribute('href', dataUri);
+    elem.setAttribute('download', fileName);
+    elem.click();
+    elem.remove();
+  }
+  public triggerImport () : void {
+    // Create temporary input element
+    let elem:HTMLInputElement;
+    // Remove any hanging temporary elements
+    elem = document.getElementById("import-proxy") as HTMLInputElement;
+    if (elem) { elem.remove(); }
+    // Create and click temporary file
+    elem = document.createElement('input');
+    elem.setAttribute('type', 'file');
+    elem.setAttribute('accept', '.candle.json');
+    elem.click();
+    // When files are input
+    elem.onchange = (() => {
+      if (elem.files.length > 0) {
+        // New file reader
+        let reader = new FileReader();
+        // What do do when a file is read
+        reader.onload = ((e) => {
+          let target = e.target as FileReader;
+          this.storeService.setData(JSON.parse(target.result));
+          this.sheet = this.storeService.getSheet();
+        });
+        // Read file
+        reader.readAsText(elem.files[0]);
+        // Remove temporary element
+        elem.remove();
+      }
+    });
   }
 }
--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 
 import { Store } from './store';
-import data from './data-example';
+import { Sheet } from './sheet/sheet';
+import dataExample from './data-example';
 
 @Injectable()
 export class StoreService {
@@ -9,10 +10,16 @@ export class StoreService {
   private store:Store;
   // Initialization
   public constructor () {
-    this.store = Object.assign({}, data);
+    this.setData(dataExample);
   }
   // Getters and setters
   public getSheet () : Sheet {
     return this.store.sheet;
+  }
+  public getData () : Store {
+    return this.store;
+  }
+  public setData (data:Store) : void {
+    this.store = data;
   }
 }
