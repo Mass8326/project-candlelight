14890a4b7027f7ebfc7f644007ef97d8d6d6d8a7
Mass8326 <reyf.mag@gmail.com>
2019-01-30 18:36:19 -0500

    Add keyboard trap on modal windows
    All edit windows will now have the first field selected on open.

--- src/app/modal/modal-edit.component.ts
+++ src/app/modal/modal-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, HostListener } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 import { Spell } from '../store';
@@ -18,10 +18,16 @@ export class ModalEditComponent implements OnInit {
     info: new FormControl(''),
     desc: new FormControl(''),
   });
+  // Focus and keyboard traps
+  @ViewChild("first") firstFocus;
+  @ViewChild("last") lastFocus;
+
   // Dependency injection
   public constructor ( private eventService:EventService ) { }
+
   // Initialization
   public ngOnInit () : void {
+    // Initialize form values
     this.spellForm.controls.name.setValue(this.editProps.spell.name);
     this.spellForm.controls.memo.setValue(this.editProps.spell.memo);
     let temp;
@@ -35,6 +41,31 @@ export class ModalEditComponent implements OnInit {
       temp += '\n' + this.editProps.spell.desc[i];
     }
     this.spellForm.controls.desc.setValue(temp);
+    // Set default focus
+    // setTimeout is needed to override default browser behavior
+    setTimeout(() => { this.firstFocus.nativeElement.select(); }, 1);
+  }
+  // Keyboard traps
+  @HostListener('window:keydown', ['$event'])
+  private keyboardTrap(event:KeyboardEvent) {
+    // Forward tab trap
+    if(event.keyCode === 9 && !event.shiftKey &&
+      document.activeElement === this.lastFocus.nativeElement) {
+      // setTimeout is needed to override default browser behavior
+      this.firstFocus.nativeElement.scrollTop = 100;
+      setTimeout(() => { this.firstFocus.nativeElement.select(); }, 1);
+    }
+    // Backward tab trap
+    if(event.keyCode === 9 && event.shiftKey &&
+      document.activeElement === this.firstFocus.nativeElement) {
+      // setTimeout is needed to override default browser behavior
+      setTimeout(() => { this.lastFocus.nativeElement.focus(); }, 1);
+    }
+    // Escape
+    if(event.keyCode === 27) {
+      this.onExit();
+      console.log("boop");
+    }
   }
   // Event handlers
   public onSave () : void {
